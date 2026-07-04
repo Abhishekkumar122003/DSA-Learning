35bf79bc1de6251fbf1fe7e497a6306345be59a8
Abhishek Kumar <abhishek801503gupta@gmail.com>
2026-07-04 08:36:26 +0000

    still not solved

--- E_Frequency_of_Element_X.cpp
+++ E_Frequency_of_Element_X.cpp
@@ -5,20 +5,39 @@ using namespace std;
 signed main(){
     int n , q;
     cin>>n>>q;
-    map<int , int> mp;
+    // map<int , int> mp;
+    int arr[n];
     for(int i =0; i<n; i++){
-        int num;
-        cin>>num;
-        mp[num]++;
+       cin>>arr[i];
     }
-
+        for(int i=0; i<n; i++){
+            int mn=arr[i];
+            int location=i;
+            for(int j=j+1; j<n; j++){
+                if(mn>arr[j]){
+                    mn=arr[j]; location=j;
+                }
+            }
+            swap(arr[i] , arr[location]);
+        }
     while(q--){
         int x;
         cin>>x;
-        if( mp.find(x) != mp.end() ){
-            cout<<mp[x]<<"\n";
+       int s=0 ;int  e=n-1;
+       int a=-1;
+       while(s<=e){
+        int mid= s+(s - e)/2;
+        if(arr[mid]>=x){
+            a=mid;
+            e=mid-1;
+        }else{
+            s=mid+1;
+        }
+        if(a!=-1){
+        cout<<n-a+1<<"\n";
         }else{
             cout<<0<<"\n";
         }
+       }
     }
 }
