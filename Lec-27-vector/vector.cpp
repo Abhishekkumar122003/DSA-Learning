c79ad33c4421b8719e6955a7a52da2e4a9704746
Abhishek Kumar <abhishek801503gupta@gmail.com>
2026-07-11 09:55:49 +0000

    notice how the size and capacity looks same but after 3 element pushed  capacity  > size.

---
+++ Lec-27-vector/vector.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+
+    vector<int> v;
+
+    //Size and Capacity of the vector
+    cout<<"Size of the Vector 'V' "<< v.size()<<"\n";
+    cout<<"Capacity of the Vector 'V' "<< v.capacity()<<"\n";
+
+    v.push_back(1);
+    cout<<"Size of the Vector 'V' "<< v.size()<<"\n";
+    cout<<"Capacity of the Vector 'V' "<< v.capacity()<<"\n";
+
+    v.push_back(2);
+    cout<<"Size of the Vector 'V' "<< v.size()<<"\n";
+    cout<<"Capacity of the Vector 'V' "<< v.capacity()<<"\n";
+
+    v.push_back(2);
+    cout<<"Size of the Vector 'V' "<< v.size()<<"\n";
+    cout<<"Capacity of the Vector 'V' "<< v.capacity()<<"\n";  
+}
