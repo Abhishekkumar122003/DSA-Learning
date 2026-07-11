0e290fd4a5f7c18ad5796761e4f2abc0dbd1cbed
Abhishek Kumar <abhishek801503gupta@gmail.com>
2026-07-11 10:07:17 +0000

    .

--- Lec-27-vector/vector.cpp
+++ Lec-27-vector/vector.cpp
@@ -3,23 +3,32 @@
 using namespace std;
 int main(){
 
-    vector<int> v;
+    // vector<int> v;
 
-    //Size and Capacity of the vector
-    cout<<"Size of the Vector 'V' "<< v.size()<<"\n";
-    cout<<"Capacity of the Vector 'V' "<< v.capacity()<<"\n";
+    // //Size and Capacity of the vector
+    // cout<<"Size of the Vector 'V' "<< v.size()<<"\n";
+    // cout<<"Capacity of the Vector 'V' "<< v.capacity()<<"\n";
 
-    v.push_back(1);
-    cout<<"Size of the Vector 'V' "<< v.size()<<"\n";
-    cout<<"Capacity of the Vector 'V' "<< v.capacity()<<"\n";
+    // v.push_back(1);
+    // cout<<"Size of the Vector 'V' "<< v.size()<<"\n";
+    // cout<<"Capacity of the Vector 'V' "<< v.capacity()<<"\n";
 
-    v.push_back(2);
-    cout<<"Size of the Vector 'V' "<< v.size()<<"\n";
-    cout<<"Capacity of the Vector 'V' "<< v.capacity()<<"\n";
+    // v.push_back(2);
+    // cout<<"Size of the Vector 'V' "<< v.size()<<"\n";
+    // cout<<"Capacity of the Vector 'V' "<< v.capacity()<<"\n";
 
-    v.push_back(2);
-    cout<<"Size of the Vector 'V' "<< v.size()<<"\n";
-    cout<<"Capacity of the Vector 'V' "<< v.capacity()<<"\n";   //here Capacity =4 Size =3;
+    // v.push_back(2);
+    // cout<<"Size of the Vector 'V' "<< v.size()<<"\n";
+    // cout<<"Capacity of the Vector 'V' "<< v.capacity()<<"\n";   //here Capacity =4 Size =3;
+    
+    // vector<int> v1(5 , 1);
+    // cout<<"Size of the Vector 'V1' "<< v1.size()<<"\n";
+    // cout<<"Capacity of the Vector 'V1' "<< v1.capacity()<<"\n";
+    // v1.push_back(3);
+    // cout<<"Size of the Vector 'V1' "<< v1.size()<<"\n";
+    // cout<<"Capacity of the Vector 'V1' "<< v1.capacity()<<"\n";  // here Capacity =10 Size =6
 
+    vector<int> v2 = {1 , 2 , 3 , 4};
+    cout<<v2.size()<<"\n";
     
 }
