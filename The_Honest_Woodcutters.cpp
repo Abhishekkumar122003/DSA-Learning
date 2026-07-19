0d946988f89a4a7db1960ecf06f39ea1b9f74ce5
Abhishek Kumar <abhishek801503gupta@gmail.com>
2026-07-19 18:49:24 +0000

    tring to solve this problem

--- The_Honest_Woodcutters.cpp
+++ The_Honest_Woodcutters.cpp
@@ -2,5 +2,17 @@
 using namespace std;
 int main(){
     int n;
-    int A[n] , B[n];
+    int A[n+1] , B[n+1];
+
+    for(int i=1; i<=n; i++){
+        cin>>A[i];
+    }
+    for(int i=1; i<=n; i++){
+        cin>>B[i];
+    }
+    for(int i=1; i<=n;i++){
+        if(B[A[i]] == i){
+            flag= true;
+        }
+    }
 }
