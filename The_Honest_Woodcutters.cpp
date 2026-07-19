6baab58f27be9630245837159b78f65e69840409
Abhishek Kumar <abhishek801503gupta@gmail.com>
2026-07-19 18:51:23 +0000

    solved it

--- The_Honest_Woodcutters.cpp
+++ The_Honest_Woodcutters.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 int main(){
     int n;
+    cin>>n;
     int A[n+1] , B[n+1];
 
     for(int i=1; i<=n; i++){
@@ -10,9 +11,18 @@ int main(){
     for(int i=1; i<=n; i++){
         cin>>B[i];
     }
+    bool flag = true;
     for(int i=1; i<=n;i++){
         if(B[A[i]] == i){
             flag= true;
+        }else{
+            flag= false;
+            break;
         }
+    }
+    if(flag == true){
+        cout<<"Yes"<<"\n";
+    }else{
+        cout<<"No"<<"\n";
     }
 }
