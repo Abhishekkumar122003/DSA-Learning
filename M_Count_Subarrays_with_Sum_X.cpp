667247315d398657f809cfa74aad067a6bcfa534
Abhishek Kumar <abhishek801503gupta@gmail.com>
2026-07-26 22:13:22 +0000

    solved it

---
+++ M_Count_Subarrays_with_Sum_X.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#define int long long
+using namespace std;
+signed main(){
+    int n, tar;
+    cin>>n>>tar;
+    map<int , int> mp;
+    int pre_sum =0 , counter=0;
+    for(int i=1; i<=n; i++){
+        int num;
+        cin>>num;
+        pre_sum += num;
+        // check is pre_sum == target=>counter++
+        if(pre_sum == tar){
+            counter++;
+        }
+        
+        int req = pre_sum - tar;
+        //check if mp.contain(req)=>counter++
+        if(mp.find(req) != mp.end()){
+            counter +=mp[req];;
+        }
+
+        if(mp.find(pre_sum) != mp.end()){
+            mp[pre_sum]++;
+        }else{
+            mp[pre_sum]=1;
+        }
+        
+    }
+    // for(auto x: mp){
+    //     cout<<x<<" ";
+    // }
+    cout<<counter;
+    return NULL;
+}
