8855fc41a1a2711a27c8bc332df41e0378e36c2e
Abhishek Kumar <abhishek801503gupta@gmail.com>
2026-07-26 10:03:26 +0000

    solved

--- G_Intersection_of_Two_Arrays_II.cpp
+++ G_Intersection_of_Two_Arrays_II.cpp
@@ -5,14 +5,36 @@ using namespace std;
 signed main(){
     int n , m;
     cin>>n;
-    int A[n];
-    for(int i=0; i<n; i++){
-        cin>>A[i];
+    //step 1 just stored the key value pair
+    unordered_map<int , int> mp;
+  for(int i=1; i<=n; i++){
+    int num;
+    cin>>num;
+    if(mp.find(num) != mp.end()){
+        mp[num]++;
+    }else{
+        mp[num]=1;
     }
-    cin>>m;
-    int B[m];
-    for(int i=0; i<m; i++){
-        cin>>B[i];
+  }
+  
+  // compaire the key of B's arrays element if present in map-> vec.push_back() and decrease it' frequency by 1 => if-> greater the 0; else=> do nothing
+  cin>>m;
+  vector<int> vec;
+  for(int i=1; i<=m; i++){
+    int num;
+    cin>>num;
+    if(mp.find(num) != mp.end()){
+        if(mp[num]>0){
+            vec.push_back(num);
+            mp[num]--;
+        }
     }
-    map<int , int>mp_1 , mp_2;
+  }
+//    store the vec size for the frequency of the intersection ans sort the "vec" the print the element
+  int k= vec.size();
+  cout<<k<<"\n";
+  sort(vec.begin() , vec.end());
+  for(auto x: vec){
+    cout<<x<<" ";
+  }
 }
