25ee1f2f2193ab8594cdeb4dfd22a66459f94f64
Abhishek Kumar <abhishek801503gupta@gmail.com>
2026-07-26 20:18:29 +0000

    re-allocated to there folder

--- 100XBootcamp/Assignment/Week 10 Set and Map/II.cpp
+++ 100XBootcamp/Assignment/Week 10 Set and Map/II.cpp
@@ -1,39 +1,33 @@
-#include<bits/stdc++.h>
 #include<iostream>
+#include<bits/stdc++.h>
 #define int long long
 using namespace std;
 signed main(){
-    int q;
-    cin>>q;
+    int n, x;
+    cin>>n>>x;
     map<int , int> mp;
-    while(q--){
-        int typ;
-        cin>>typ;
-        if(typ == 1){
-            int num;
-            cin>>num;
-            mp[num]++;
-        }else if(typ == 2){
-            int num;
-            cin>>num;
-            if(mp.find(num)!= mp.end()){
-              mp[num]--;
-              if(mp[num] == 0){
-                mp.erase(num);
-              }  
-            }
-           
-         
-        }else if(typ == 3){
-            cout<<mp.size()<<"\n";
-        }else {
-            int num;
-            cin>>num;
-            if(mp.find(num) != mp.end()){
-                cout<<"YES"<<"\n";
-            }else{
-                cout<<"NO"<<"\n";
-            }
+    int target;
+    int second_occurence;
+    bool flag= false;
+    for(int i=1; i<=n; i++){
+        int num;
+        cin>>num;
+        target =x-num;
+        if(mp.find(target) != mp.end()){
+            second_occurence=i;
+            flag = true;
+            break;
         }
+        if(mp.find(num) != mp.end()){
+            mp[num] =i;
+        }else{
+            mp[num] =i;
+        }
+    }
+
+    if(flag == true){
+        cout<<mp[target]<<" "<<second_occurence;
+    }else{
+        cout<<-1;
     }
 }
