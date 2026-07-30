c5a8fa07a9e5510a00cb1a2d6055c2f9518dfc87
Abhishek Kumar <abhishek801503gupta@gmail.com>
2026-07-30 06:16:28 +0000

    it pass the given test case

--- E_Longest_Subarray_with_Sum_K.cpp
+++ E_Longest_Subarray_with_Sum_K.cpp
@@ -8,6 +8,18 @@ signed main(){
     for(int i=0; i<n;i++){
         cin>>arr[i];
     }
-   
-    
+   //initialize the left and right to 0 and create sum ans max_length variable with 0 initialize as value
+   int left=0 , right=0;
+   int sum=0; int max_length=0;
+   for(right; right<n; right++){
+    sum +=arr[right];
+    //this line runs when the Sum value is greater then target(X), and run until the sum< target by decreasing the value of arr[left]; left++ 
+    while(sum>=k){
+        sum -= arr[left];
+        left++;
+    }
+    max_length = max(max_length , right-left+1); // this "right - left + 1"-> gives you the current window length
+   }
+   cout<<max_length;
+
 }
