ada60f15d81eb78c1ba7082627058fa7e7e51496
Abhishek Kumar <abhishek801503gupta@gmail.com>
2026-07-30 06:06:48 +0000

    i take help to solve this problem

--- E_Longest_Subarray_with_Sum_K.cpp
+++ E_Longest_Subarray_with_Sum_K.cpp
@@ -8,26 +8,6 @@ signed main(){
     for(int i=0; i<n;i++){
         cin>>arr[i];
     }
-    int window=2;
-    int ans;
-    while(window<n){
-        int sum=0;
-        for(int i=0; i<window; i++){
-            sum+=arr[i];
-        }
-        if(sum<k){
-            ans=window;
-        }
-        int left=0 , right=window;
-        while(right<n){
-        sum+=arr[right] - arr[left];
-        if(sum<k){
-            ans=window;
-        }
-        right++;
-        left++;
-    }
-    window++;
-    }
-    cout<<ans;
+   
+    
 }
