c9ebec6d974324984513124957368fd81908af47
Abhishek Kumar <abhishek801503gupta@gmail.com>
2026-08-02 18:42:28 +0000

    It passed the given test case

--- I_Count_Subarrays_with_At_Most_K_Distinct.cpp
+++ I_Count_Subarrays_with_At_Most_K_Distinct.cpp
@@ -9,4 +9,21 @@ signed main(){
     for(int i=0; i<n; i++){
         cin>>arr[i];
     }
+    map<int , int> mp;
+    int left =0, count_sub_arr=0;
+    for(int right =0; right<n; right++){
+        mp[arr[right]]++;
+        while(mp.size()>k){
+           
+                mp[arr[left]]--;
+            if(mp[arr[left]] ==0){
+                mp.erase(arr[left]);
+            }
+            
+                left++;
+
+        }
+        count_sub_arr += right - left +1;
+    }
+    cout<<count_sub_arr;
 }
