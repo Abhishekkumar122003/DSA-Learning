b094fe388b31446feba41d01b3a848e4d563545c
Abhishek Kumar <abhishek801503gupta@gmail.com>
2026-08-02 17:56:51 +0000

    passes all test case and accepted

--- H_Count_Subarrays_with_Sum_K.cpp
+++ H_Count_Subarrays_with_Sum_K.cpp
@@ -9,22 +9,16 @@ signed main(){
     for(int i=0; i<n; i++){
         cin>>arr[i];
     }
-    map<int ,int> mp;
+    
     int left=0 ; 
     int count=0, sum=0;
     for(int right=0; right<n; right++){
         sum += arr[right];
-        if(sum<k){
-            count +=1;
-        }else{
-            while(sum>=k){
-                sum -= arr[left];
-                if(sum<k){
-                    count +=1;
-                }
-                left++;
-            }
+        while(sum >= k){
+            sum -= arr[left];
+            left++;
         }
+        count += right - left + 1;
     }
     cout<<count;
 }
