286fe549ce556d5c0d6753c609b9685b88002cfb
Abhishekkumar122003 <abhishek801503gupta@gmail.com>
2026-08-15 20:48:09 +0530

    trying to implement cyclic sort algorithm

--- a.cpp
+++ a.cpp
@@ -1,6 +1,18 @@
 #include<iostream>
 using namespace std;
 int main(){
-    cout<<"hi";
-    cout<<"\n";
+   int a[9]={2,5,1,0,3,0,1,2,5};
+   for(int i=0; i<9; i++){
+    if(i != a[i]){
+        while(i!=a[i]){
+            cout<<"hi";
+            swap(a[i], a[a[i]]);
+        }
+    }
+   }
+   for(int i=0; i<9; i++){
+    if(i!= a[i]){
+        cout<<a[i]<<"\n";
+    }
+   }
 }
