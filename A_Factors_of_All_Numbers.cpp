29431dc23f7e50cdf49a821b039da986de67150a
Abhishekkumar122003 <abhishek801503gupta@gmail.com>
2026-08-22 15:29:57 +0530

    with sieve algorithm it pass within time limit

--- A_Factors_of_All_Numbers.cpp
+++ A_Factors_of_All_Numbers.cpp
@@ -1,29 +1,47 @@
 #include<iostream>
-#define int long long
+#include<bits/stdc++.h>
+// #define int long long
 using namespace std;
-void factors(int b){
-    //implementing the factorial logic
-    for(int i=1; i<=b; i++){
-        int num=i;
+// void factors(int b){
+//     //implementing the factorial logic
+//     for(int i=1; i<=b; i++){
+//         int num=i;
         
-        int j=1;
-        while(j*j<=num){
-            if(num%j==0){
-                cout<<j<<" ";
-            }
-            j++;
-        }
-        j--;
-        while(j>=1){
-            if(num%j ==0){
-                if(num/j != j){
-                    cout<<num/j<<" ";
-                }
-            }
-            j--;
-        }
+//         int j=1;
+//         while(j*j<=num){
+//             if(num%j==0){
+//                 cout<<j<<" ";
+//             }
+//             j++;
+//         }
+//         j--;
+//         while(j>=1){
+//             if(num%j ==0){
+//                 if(num/j != j){
+//                     cout<<num/j<<" ";
+//                 }
+//             }
+//             j--;
+//         }
 
-        cout<<"\n";
+//         cout<<"\n";
+//     }
+// 
+void factors(int b){
+    vector<int> factor[b+1];
+    for(int i=1; i<=b; i++){
+        for(int j=i; j<=b; j+=i){
+            //if "i" is factor of "j" push 'i' into factor[j]
+            if(j%i==0){
+                factor[j].push_back(i);
+        }
+        }
+    }
+    for(int i=1; i<=b; i++){
+       for(int x: factor[i]){
+        cout<<x<<" ";
+       }
+       cout<<"\n";
     }
 }
 signed main(){
