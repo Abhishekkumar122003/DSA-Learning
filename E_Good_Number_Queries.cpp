5b064188e9e53a46ea88e7a72b28fa40c5884fec
Abhishekkumar122003 <abhishek801503gupta@gmail.com>
2026-08-24 18:16:17 +0530

    origalnal proble is asking about the num of factors is prime or not

--- E_Good_Number_Queries.cpp
+++ E_Good_Number_Queries.cpp
@@ -1,40 +1,32 @@
 #include<iostream>
 using namespace std;
 const int N=1e6;
-int goodNum[N+1];
-void pre_compute(){
-    for(int i=0; i<=N; i++){
-        goodNum[i]=i;
-    }
 
-    for(int i=2; i<=N; i++){
-        for(int j=i*2; j<=N; j+=i){
-            goodNum[j]=min(goodNum[j] , i);
-        }
-    }
+int factors[N +1];
 
-}
-void Good_Prime(){
-    int q;
-    cin>>q;
-    while(q--){
-        int x;
-        cin>>x;
-        if(x==0||x==1){
-            cout<<"NO"<<"\n";
-        }else{
-            if(goodNum[x] == goodNum[goodNum[x]]){
-                cout<<"YES"<<"\n";
-            }else{
-                cout<<"NO"<<"\n";
-            }
+void pre(){
+    for(int i=1; i<=N; i++){
+        for(int j=i; j<=N; j+=i){
+            factors[j]++;
         }
     }
 }
-int main(){
-    ios_base::sync_with_stdio(false);
+bool isPrime(int n){
+    return factors[n] == 2;
+}
+void solve(){
+    int x;
+    cin>>x;
+    cout<<(isPrime(factors[x]) ? "YES" : "NO") << "\n";
+}
+signed main(){
+    ios_base:: sync_with_stdio(false);
     cin.tie(NULL);
+    pre();
 
-    pre_compute();
-    Good_Prime();
+    int t=1;
+    cin>>t;
+    while(t--){
+        solve();
+    }
 }
