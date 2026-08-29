e6cc78e094e2d6356a88976f59dd28d991e62cdd
Abhishekkumar122003 <abhishek801503gupta@gmail.com>
2026-08-29 11:10:35 +0530

    12th done

---
+++ Expert_Setter.cpp
@@ -0,0 +1,26 @@
+  #include<iostream>
+  using namespace std;
+
+  void solve(){
+    float x,y;
+    cin>>x>>y;
+
+    if(x/2 <= y){
+        cout<<"YES"<<"\n";
+    }else{
+        cout<<"NO"<<"\n";
+    }
+
+  }
+
+  int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        solve();
+    }
+    
+  }
