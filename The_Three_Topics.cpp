55346878c87187c7f9c848983fee4109112fd1c7
Abhishekkumar122003 <abhishek801503gupta@gmail.com>
2026-08-29 15:54:45 +0530

    19th

---
+++ The_Three_Topics.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+ void solve(){
+    int a, b,c,x;
+    cin>>a>>b>>c>>x;
+    if(a == x || b == x || c == x){
+        cout<<"Yes"<<endl;
+    }else{
+        cout<<"No"<<"\n";
+    }
+ }
+
+ int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    
+        solve();
+    
+ }
