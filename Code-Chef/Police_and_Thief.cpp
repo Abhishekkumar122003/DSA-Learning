38be3d051cae9fe2546c008048b16d58b9591b69
Abhishekkumar122003 <abhishek801503gupta@gmail.com>
2026-09-01 01:14:46 +0530

    36th

---
+++ Code-Chef/Police_and_Thief.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+void solve(){
+    int x,y;
+   cin>>x>>y;
+    cout<<abs(x-y)<<"\n";
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--){
+         solve();
+    }
+}
