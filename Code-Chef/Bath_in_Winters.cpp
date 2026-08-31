02efdb5cefff33be7b31dee91519e51066a68bb9
Abhishekkumar122003 <abhishek801503gupta@gmail.com>
2026-09-01 01:32:51 +0530

    38th

---
+++ Code-Chef/Bath_in_Winters.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+void solve(){
+    int x,y;
+   cin>>x>>y;
+   cout<<x/(2*y)<<endl;
+   
+    
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--){
+         solve();
+    }
+}
