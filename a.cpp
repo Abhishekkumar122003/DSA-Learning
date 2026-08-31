1d7519c818e19b7d105d3888fdc761c3b12d6594
Abhishekkumar122003 <abhishek801503gupta@gmail.com>
2026-08-31 17:34:00 +0530

    resume

--- a.cpp
+++ a.cpp
@@ -1,20 +1,15 @@
 #include<iostream>
 using namespace std;
 int main(){
-   int a[9]={2,5,1,0,3,0,1,2,5};
-   for(int i=0; i<9; i++){
-    int currentele=a[i];
-    int ele=a[i];
-    if(a[currentele] != ele){
-        
-            cout<<"hi";
-            swap(a[i], a[currentele]);
-            i--;
-    }
+   int num=2;
+   for (int i = 2; i <= 64; i++)
+   {
+       if(num <= 64){
+           cout<<num<<" \n";
+       }else{
+           break;
+       }
+    num *= 2;
    }
-   for(int i=0; i<9; i++){
-    if(i!= a[i]){
-        cout<<a[i]<<"\n";
-    }
-   }
+   
 }
