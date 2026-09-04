3e05b271ddc931f1654d9c6cfa72a17544318ed6
Abhishekkumar122003 <abhishek801503gupta@gmail.com>
2026-09-04 23:30:56 +0530

    46th

---
+++ Candy_Distribution.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+ using namespace std;
+ void solov(){
+    int N,M;
+    cin>>N>>M;
+     
+
+
+    cout<<"\n";
+}
+
+ int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--){
+        solov();
+    }
+ }
