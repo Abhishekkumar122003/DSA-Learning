161146ec0cbffc8f2a61ef159b841d85a7f40671
Abhishekkumar122003 <abhishek801503gupta@gmail.com>
2026-09-04 23:24:28 +0530

    '

---
+++ Chef_and_Water_Bottles.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+ using namespace std;
+ void solov(){
+    int N,X,K;
+    cin>>N>>X>>K;
+    int totalBottle = K/X;
+    if(totalBottle >= N){
+        cout<<N;
+    }else{
+        cout<<
+    }
+    cout<<"\n";
+}
+
+ int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    while(t--){
+        solov();
+    }
+ }
