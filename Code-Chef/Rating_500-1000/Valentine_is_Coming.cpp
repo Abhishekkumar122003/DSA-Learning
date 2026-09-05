09f9e9b2349ed3000361087b4827919ef21f7946
Abhishekkumar122003 <abhishek801503gupta@gmail.com>
2026-09-05 11:53:46 +0530

    53th i don't expect that CChef asked this easy question at 691 rating

---
+++ Code-Chef/Rating_500-1000/Valentine_is_Coming.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    int X, Y;
+    cin>>X>>Y;
+    cout<<X/Y<<"\n";
+}
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin>>t;
+    while(t--){
+        solve();
+    }
+}
