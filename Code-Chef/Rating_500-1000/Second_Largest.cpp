9957a9aafe8b328b57c2da9e9003c8a512b6d891
Abhishekkumar122003 <abhishek801503gupta@gmail.com>
2026-09-18 01:26:31 +0530

    more efficient way

--- Code-Chef/Rating_500-1000/Second_Largest.cpp
+++ Code-Chef/Rating_500-1000/Second_Largest.cpp
@@ -5,16 +5,19 @@ using namespace std;
 void solve(){
    int a, b, c;
    cin>>a>>b>>c;
-   int MAX = max({a,b,c});
-   if(a == MAX){
-    cout<<max(b,c);
-   }else if(b == MAX){
-    cout<<max(a,c);
-   }else{
-    cout<<max(a,b);
-   }
-   cout<<endl;
-
+//    int MAX = max({a,b,c});
+//    if(a == MAX){
+//     cout<<max(b,c);
+//    }else if(b == MAX){
+//     cout<<max(a,c);
+//    }else{
+//     cout<<max(a,b);
+//    }
+//    cout<<endl;
+    
+//tricks => second = a+b+c -min({a,b,c}) - max({a,b,c})
+int second = a + b + c - min({a, b, c}) - max({a , b, c});
+cout<<second<<"\n";
 
 
 }
